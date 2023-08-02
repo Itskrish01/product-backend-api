ed786daaef2282d6041135b76f781266b3114219
itsKrish01 <72066631+Itskrish01@users.noreply.github.com>
2023-08-02 16:04:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product"); // Assuming you have a product model defined
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
+
+app.use(express.json());
+
+// Routes go here
+app.get("/", (req, res) => {
+  res.send({ title: "Products API" });
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ error: "Something went wrong." });
+  }
+});
+
+// Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Listening for requests");
+  });
+});
---
+++ models/product.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ProductSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  discountPercentage: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
