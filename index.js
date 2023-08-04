8a77ae309abf625ef69967a496eefa9f17414a19
itsKrish01 <72066631+Itskrish01@users.noreply.github.com>
2023-08-04 11:41:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const Product = require("./models/product"); // Assuming you have a product model defined
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 mongoose.set("strictQuery", false);
 const connectDB = async () => {
