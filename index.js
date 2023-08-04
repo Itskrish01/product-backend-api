039d659f5af1f130ff08d73d7b233dac6f4f6ca2
itsKrish01 <72066631+Itskrish01@users.noreply.github.com>
2023-08-04 11:50:51 +0530

    Added stripe endpoint

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Product = require("./models/product"); // Assuming you have a product model defined
+const { default: Stripe } = require("stripe");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 mongoose.set("strictQuery", false);
 const connectDB = async () => {
@@ -90,6 +93,49 @@ app.post("/products/add", async (req, res) => {
     res.status(201).json({ message: "Product added successfully" });
   } catch (error) {
     console.error("Error adding product:", error);
+    res.status(500).json({ error: "Something went wrong." });
+  }
+});
+
+// Stripe endpoint to create a Checkout Session
+app.post("/stripe", async (req, res) => {
+  try {
+    const { _id } = req.body; // Assuming you send the product ID in the request body
+
+    // Fetch the product details from the database
+    const product = await Product.findById(_id);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    // Convert the price from USD to INR (using an exchange rate or API)
+    const exchangeRate = 73.5; // Example exchange rate (1 USD to INR)
+    const priceInINR = product.price * exchangeRate;
+
+    // Create a Stripe Checkout Session
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "inr", // Use INR as the currency for Stripe
+            product_data: {
+              name: product.title,
+              images: [product.thumbnail],
+            },
+            unit_amount: Math.round(priceInINR * 100), // Convert to paisa (smallest currency unit in INR)
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `https://nextjs-ecommerce-app-five.vercel.app//success`,
+      cancel_url: `https://nextjs-ecommerce-app-five.vercel.app//canceled`,
+    });
+
+    res.status(201).json({ sessionId: session.id });
+  } catch (error) {
+    console.error("Error creating Stripe session:", error);
     res.status(500).json({ error: "Something went wrong." });
   }
 });
